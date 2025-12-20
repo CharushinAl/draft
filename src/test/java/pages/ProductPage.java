6ce933005cadb4b3094059548cc22f67b437e9f8
sasha <charushinas@gmail.com>
2025-12-20 11:34:29 +0300

    added BasePage, changed other Pages

--- src/test/java/pages/ProductPage.java
+++ src/test/java/pages/ProductPage.java
@@ -2,16 +2,18 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class ProductPage {
+public class ProductPage extends BasePage {
     WebDriver driver;
     By pageTitle = By.xpath("//*[@data-test='title']");
 
     public ProductPage(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
     public boolean isPageLoaded() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(pageTitle));
         return driver.findElement(pageTitle).isDisplayed();
     }
 }
